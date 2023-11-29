3bc49e160597a27379294865de9e970417ad78ec
Ovmiguel16 <omanavillamizarmian@uvm.edu.ve>
2023-11-29 01:14:35 -0400

    Tercera entidad | Opinion del torneo

---
+++ src/models/opinionModel.js
@@ -0,0 +1,29 @@
+const { Schema, model} = require('mongoose');
+
+// Entidad para que los usuarios puedan opinar y puntuar la experiencia del torneo.
+
+const opinionSchema = new Schema({
+    opinion: { // POST | GET | PUT | DELETE
+        type: String,
+        trim: true
+    },
+    expectativa: { 
+        type: String,
+        trim: true,
+    },
+    calificacion: { 
+        type: String,
+        trim: true,
+    },
+    usuarioId:{
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+    },  
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+module.exports = model('opinion', opinionSchema);
